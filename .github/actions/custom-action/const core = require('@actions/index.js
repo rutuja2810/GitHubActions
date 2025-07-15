d5654a897056be900fe953bcc3f575c5dab099fe
rutuja2810 <92105785+rutuja2810@users.noreply.github.com>
2025-07-15 18:30:52 +0530

    Delete .github/actions/custom-action/const core = require('@actions directory

--- .github/actions/custom-action/const core = require('@actions/index.js
+++
@@ -1,8 +0,0 @@
-const core = require('@actions/core');
-
-try {
-  const name = core.getInput('name');
-  console.log(`Hello, ${name}! 👋`);
-} catch (error) {
-  core.setFailed(`Action failed with error: ${error}`);
-}
