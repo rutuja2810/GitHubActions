3a448d73ec59d1e2017911de4929680324a2db28
rutuja2810 <92105785+rutuja2810@users.noreply.github.com>
2025-07-22 17:55:21 +0530

    Update testShell.sh

--- scripts/testShell.sh
+++ scripts/testShell.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-echo "Enter your name:"
-read name
-echo "Welcome, $name!"
-
-echo "👋 Hello, welcome to Shell Scripting!"
-echo "📅 Today's date: $(date)"
-echo "👤 Current user: $(whoami)"
-echo "📁 You're in: $(pwd)"
-echo "🗂️ Listing files:"
-ls -l
-
 
 |
 echo $(date) >>abc.txt
